1a0788580097945c7aed1b0a9473bdb67a92d849
Schaban Bochi <s.bochi@qlevar.com>
2021-11-30 18:31:52 +0100

    cleanup

--- example/lib/screens/examples_page.dart
+++ example/lib/screens/examples_page.dart
@@ -17,34 +17,37 @@ class ExamplesPage extends StatelessWidget {
               crossAxisAlignment: WrapCrossAlignment.start,
               alignment: WrapAlignment.center,
               direction: Axis.vertical,
-              children: [
-                Description(
-                    "Repo",
-                    () => launch('https://github.com/SchabanBo/qr_samples'),
-                    'Show the sample repo on github'),
-                Description(
-                    "Dashboard",
-                    () => launch(
-                        'https://github.com/SchabanBo/qr_samples/blob/main/lib/common_cases/dashboard.dart'),
-                    'Show dashboard example'),
-                Description(
-                    "Bottom Navigation bar",
-                    () => launch(
-                        'https://github.com/SchabanBo/qr_samples/blob/main/lib/common_cases/bottom_nav_bar.dart'),
-                    'Example of bottom navigation bar with QR'),
-                Description(
-                    "Can pop",
-                    () => launch(
-                        'https://github.com/SchabanBo/qr_samples/blob/main/lib/examples/can_pop.dart'),
-                    'Example canceling the navigation with back button'),
-                Description(
-                    "Nested in Nested",
-                    () => launch(
-                        'https://github.com/SchabanBo/qr_samples/blob/main/lib/special_cases/nested_in_nested.dart'),
-                    'Complex Example of using multi nested navigations in each other'),
-              ],
+              // ignore: lines_longer_than_80_chars
+              children: children,
             )),
       ),
     );
   }
+
+  List<Widget> get children => [
+        Description(
+            "Repo",
+            () => launch('https://github.com/SchabanBo/qr_samples'),
+            'Show the sample repo on github'),
+        Description(
+            "Dashboard",
+            () => launch(
+                'https://github.com/SchabanBo/qr_samples/blob/main/lib/common_cases/dashboard.dart'),
+            'Show dashboard example'),
+        Description(
+            "Bottom Navigation bar",
+            () => launch(
+                'https://github.com/SchabanBo/qr_samples/blob/main/lib/common_cases/bottom_nav_bar.dart'),
+            'Example of bottom navigation bar with QR'),
+        Description(
+            "Can pop",
+            () => launch(
+                'https://github.com/SchabanBo/qr_samples/blob/main/lib/examples/can_pop.dart'),
+            'Example canceling the navigation with back button'),
+        Description(
+            "Nested in Nested",
+            () => launch(
+                'https://github.com/SchabanBo/qr_samples/blob/main/lib/special_cases/nested_in_nested.dart'),
+            'Complex Example of using multi nested navigations in each other'),
+      ];
 }
